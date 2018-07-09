99eb655f132ea27a066280819032c41ee671f5ca
Salem Ben Aissa <aissasell@gmail.com>
2018-07-09 08:01:28 +0100

    Ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,11 +14,20 @@ module.exports = function (grunt) {
                 files: ['dist/css/style.css'],
                 tasks: ['cssmin']
             }
+        },
+        imagemin: {
+            dynamic: {
+                expand: true,
+                cwd: 'images/',
+                src: ['**/*.{jpg,png,gif}'],
+                dest: 'dist/img'
+            }
         }
     }) 
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     grunt.registerTask('default', ['watch']);
 
